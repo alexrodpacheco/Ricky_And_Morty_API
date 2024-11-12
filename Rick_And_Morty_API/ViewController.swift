5fb991b01c536580667f189acb45d910bba5e1f5
Alex Pacheco <alexrodpacheco@gmail.com>
2024-11-12 17:16:43 -0300

    first commmit - create Xcode Project

---
+++ Rick_And_Morty_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick_And_Morty_API
+//
+//  Created by Alex Rodrigues Pacheco on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
